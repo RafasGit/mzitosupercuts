93a277dd3e3ee9b93cc28179167a78836e71f436
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-12-16 15:29:31 +0300

    react

--- like_button.js
+++ like_button.js
@@ -14,9 +14,46 @@ class LikeButton extends React.Component {
     }
 
     return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+        <form onSubmit={handleSubmit} style={styles.form}>
+        <h1>Mpesa Payment Form</h1>
+        {message && <p style={{ color: message.includes('failed') ? 'red' : 'green' }}>{message}</p>}
+        <div style={styles.formGroup}>
+          <label htmlFor="phone" style={styles.label}>Phone Number</label>
+          <input
+            type="number"
+            id="phone"
+            value={phone}
+            onChange={handlePhoneChange}
+            style={styles.input}
+            required
+          />
+        </div>
+        <div style={styles.formGroup}>
+          <label htmlFor="accountNumber" style={styles.label}>Account Number</label>
+          <input
+            type="text"
+            id="accountNumber"
+            value={accountNumber}
+            onChange={handleAccountNumberChange}
+            style={styles.input}
+            required
+          />
+        </div>
+        <div style={styles.formGroup}>
+          <label htmlFor="amount" style={styles.label}>Amount</label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={handleAmountChange}
+            style={styles.input}
+            required
+          />
+        </div>
+        <button type="submit" style={styles.button}>Pay Now</button>
+  
+  
+      </form>
     );
   }
 }
